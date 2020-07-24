70215ec8583a5c42c92f2669e55cd7bf14e964dc
Kristopher Lea <Kristopherlea187@gmail.com>
2020-07-24 16:35:01 -0500

    added external links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kristopher Lea';
-  city = 'Pottsville';
+  city = 'Pottsville, Arkansas';
   tagLine = 'Go outside your comfort zone. It will help you grow exponentially in many ways.';
-  aboutMe = 'I am a 39 year old who has found a new career in software development. I am a disciplined and self motivated person. I love to challenge myself. I believe everyone should challenge themselves in some way becauese it helps to better themselves.';
+  aboutMe = 'I am 39 years old and have found a new career in software development. I am a disciplined and self motivated person. I love to challenge myself. I believe everyone should challenge themselves in some way becauese it helps to better themselves.';
 }
