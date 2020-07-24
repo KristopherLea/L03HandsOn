95e80dcb3fa8692e4fc45f9475bfa81d409a5741
Kristopher Lea <Kristopherlea187@gmail.com>
2020-07-24 14:48:16 -0500

    changed css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mr. Bigglesworth';
+  title = 'Kristopher Lea';
+  city = 'Pottsville';
+  tagLine = 'Go outside your comfort zone. It will help you grow exponentially in many ways.';
+  aboutMe = 'I am a 39 year old who has found a new career in software development. I am a disciplined and self motivated person. I love to challenge myself. I believe everyone should challenge themselves in some way becauese it helps to better themselves.';
 }
